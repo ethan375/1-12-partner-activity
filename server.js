fb6cebd5ed18b1681b0ec957c2a7339cf8e5fd35
ethan375 <ethanebel1@gmail.com>
2018-01-12 12:03:41 -0600

    connected to mongo

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost:27017/coins', {
+  useMongoClient: true
+});
+
+mongoose.connection.once('open', ()=>{
+  console.log("connected to mongo")
+})
 
 app.get('/coins/new',(req, res)=>{
   res.render('new.ejs');
