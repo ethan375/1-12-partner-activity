eda0af4e3fd0dff21d618ce2c0713a0314abdcd6
ethan375 <ethanebel1@gmail.com>
2018-01-12 12:12:10 -0600

    for adds to database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 
 
@@ -10,14 +12,30 @@ mongoose.connect('mongodb://localhost:27017/coins', {
 
 mongoose.connection.once('open', ()=>{
   console.log("connected to mongo")
-})
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+const Coin = require('./models/coins.js');
+
 
 app.get('/coins/new',(req, res)=>{
   res.render('new.ejs');
 });
 
 app.post('/coins', (req,res)=>{
-  res.send('posted')
+  // res.send(req.body)
+  Coin.create(req.body, (err, createdCoin)=>{
+    if(err){console.log(err)
+    }else{
+      res.send(createdCoin);
+    }
+
+  })
 });
 
 
